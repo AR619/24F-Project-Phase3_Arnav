03f8cecfc0a5d01a2fd150a961d436cb36782201
Arnav Rathore <arnavrathore@Arnavs-MBP.lan>
2024-12-03 18:36:17 -0500

    skill_rec complete

---
+++ api/backend/student/job_rec_route.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+
+
+##### Blueprint ######
+
+job_recommendations = Blueprint('jobRec', __name__)
+
+
+#### CRUD OPS ######
+
+
